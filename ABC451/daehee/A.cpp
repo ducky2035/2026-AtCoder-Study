2edd87e4d4891919e8053e7e2743316650682913
Daehee Han <gkseogml4959@gmail.com>
2026-03-28 22:58:28 +0900

    A: AC

---
+++ ABC451/daehee/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  string s;
+  cin >> s;
+
+  if (s.size() % 5) cout << "No";
+  else cout << "Yes";
+  return;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  solve();
+}
