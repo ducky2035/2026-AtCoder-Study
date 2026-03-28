baf6f51d713c4faeb2088542c507f8a14eda162e
cnvxlns <cnvxlns@gmail.com>
2026-03-28 23:26:12 +0900

    A: AC

---
+++ ABC451/cnvxlns/A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+#define int long long
+#define endl "\n"
+
+using namespace std;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using vpii = vector<pii>;
+
+const int inf = 4e18;
+const int MOD = 1e9 + 9;
+
+vpii drc = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+
+pii operator+(pii A, pii B){
+    return {A.first + B.first, A.second + B.second};
+}
+pii operator-(pii A, pii B){
+    return {A.first - B.first, A.second - B.second};
+}
+
+void init(){
+
+}
+
+void solve(){
+    string s;
+    cin >> s;
+    if(s.size() % 5 == 0){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" <<endl;
+    }
+}
+
+int32_t main(){
+    cin.tie(0)->sync_with_stdio(0);
+    init();
+    int t = 1;
+    // cin >> t;
+    while(t--)
+        solve();
+
+    return 0;
+}
